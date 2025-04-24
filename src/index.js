c9212d75decc1fd03e467045be13d2d79eae6cc3
gyetger <gyetger@gmail.com>
2025-04-24 14:38:54 +0200

    Pluginok betöltése az index.js-ben

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import init from './init';
 import state from './state';
+import { miradorImageToolsPlugin } from 'mirador-image-tools';
+import downloadDialogPlugin from 'mirador-downloaddialog/es';
+
+const plugins = {
+    miradorImageToolsPlugin,
+    downloadDialogPlugin,
+}
 
 export default {
   ...init,
   ...state,
+  plugins
 };
